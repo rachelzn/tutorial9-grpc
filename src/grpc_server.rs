753210da880ffd1f958c7cd1d31721d17d12ce26
“rachelzn” <racheltjengerari@gmail.com>
2024-04-30 16:15:41 +0700

    fix: implement ChatServices in main function

--- src/grpc_server.rs
+++ src/grpc_server.rs
@@ -97,6 +97,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let payment_service = MyPaymentService::default();
     let transaction_service = MyTransactionService::default();
+    let chat_service = MyChatService::default();
 
     Server::builder()
         .add_service(PaymentServiceServer::new(payment_service))
